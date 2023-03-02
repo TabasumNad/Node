b011d186a4eb6db13c7494e5aa9c440b9067c757
TabasumNad <tmnadaf17@gmail.com>
2023-03-02 19:47:20 +0530

    first app in express node +vercel json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config()
 import express from "express";
 import { MongoClient } from "mongodb";
 import moviesRouter from './router/movies.js';
-
+// 
 const app = express();
 
 const PORT = process.env.PORT;
